a6e0011e7934c04a639403323515b7bb9cab3469
Imran Mughal <imran@mughal.com>
2025-05-25 15:48:32 -0400

    Update models __init__ to include new models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,16 @@
 from .user import User
 from .task import Task, Project, Tag
+from .time_tracking import TimeEntry
+from .recurrence import RecurrencePattern, RecurrenceType
+from .auth import RefreshToken
 
-__all__ = ["User", "Task", "Project", "Tag"]
+__all__ = [
+    "User", 
+    "Task", 
+    "Project", 
+    "Tag", 
+    "TimeEntry", 
+    "RecurrencePattern", 
+    "RecurrenceType",
+    "RefreshToken"
+]
