134907603b444af09bdbf0ee85cb127724fb7d4f
Imran Mughal <imran@mughal.com>
2025-05-25 16:34:23 -0400

    Fix model imports - Project and Tag are defined in task.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,5 @@
 from .user import User
-from .task import Task
-from .project import Project
-from .tag import Tag
+from .task import Task, Project, Tag
 from .template import TaskTemplate  # Issue #23
 
 __all__ = ['User', 'Task', 'Project', 'Tag', 'TaskTemplate']
